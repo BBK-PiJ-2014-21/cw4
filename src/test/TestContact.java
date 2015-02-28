341752b0ddce0a75c458ef9fda4db0d7e6b515c0
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-02-28 11:13:34 +0000

    updated TestContact

--- src/test/TestContact.java
+++ src/test/TestContact.java
@@ -3,6 +3,7 @@ package test;
 import main.Contact;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,17 +13,20 @@ import static org.junit.Assert.assertEquals;
  */
 public class TestContact {
     Contact foo;
+    Contact foo2;
     
     @Before
     public void setUp() {
         foo = new ContactImpl("Foo", 012345);
+        foo2 = new ContactImpl("Foo2", 012345, "Some notes about Foo2.");
     }
     
     @After
     public void tearDown() {
         foo = null;
+        foo2 = null;
     }
-    
+
     @Test
     public void testGetID() {
         assertEquals(foo.getId(), 012345);
@@ -44,8 +48,12 @@ public class TestContact {
         assertEquals(foo.getNotes(), "Some notes about Foo.");
     }
     
-    
-    
+    @Test
+    public void testAddNotesInTwoTimes() {
+        foo2.addNotes("More notes.");
+        foo2.addNotes("Some more notes.");
+        assertEquals(foo2.getNotes(), "Some notes about Foo2. More notes. Some more notes.");
+    }
     
     
     
