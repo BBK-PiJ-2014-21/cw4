af59581cac9c43c96a532061e5afdd62d9e91575
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-02-28 15:21:34 +0000

    added class IdCreatorImpl

---
+++ src/main/IdCreatorImpl.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.io.Serializable;
+
+/**
+ * Implementation of {@see IdCreator}. 
+ * It issues unique IDs, from 1000 upwards (to try and maintain a four-digits standard).
+ * 
+ * @author federico.bartolomei (BBK-PiJ-2014-21) 
+ */
+public class IdCreatorImpl implements IdCreator {
+    private int id = 999;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return an unique id.
+     */
+    public int createId() {
+        id++;
+        return id;
+    }
+    
+}    
+
