d0cdf20700e58579fdaa8e9bc31676fe17bc1a2e
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-03-03 15:48:44 +0000

    updated class to test createMeetingId()

--- src/test/TestIdCreator.java
+++ src/test/TestIdCreator.java
@@ -1,6 +1,11 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * JUnit test class for interface {@see IdCreator}. It assumes an implementation called IdCreatorImpl.
@@ -13,13 +18,13 @@ public class TestIdCreator {
         IdCreator id = new IdCreatorImpl();
         int[] array = new int[n];
         for(int i=0; i<array.length; i++) {
-            array[i] = id.createId();
+            array[i] = id.createContactId();
         }
         return array;
     }
     
     @Test
-    public void test100IDsAreUnique() {
+    public void test100ContactIDsAreUnique() {
         int[] array = createIdArray(100);
         for(int i=0; i<array.length; i++) {
             for(int j=i+1; j<array.length; j++) {
@@ -32,12 +37,22 @@ public class TestIdCreator {
     public void testTwoArraysOfIDsCreatedWithTwoInstancesOfIdCreatorShouldHaveTheSameIDs() {
         int[] first = createIdArray(10);
         int[] second = createIdArray(10);
-        for(int i=0; i<first.length; i++) {
-            for (int j=0; j<second.length; j++) {
-                assertFalse(first[i] + "!=" + second[j], first[i]!=second[j]);
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                assertFalse(first[i] + "!=" + second[j], first[i] != second[j]);
             }
         }
+
+    }
         
+    @Test
+    public void testMeetingId() {
+        IdCreator test = new IdCreatorImpl();
+        Set<Integer> meetingIDs = new HashSet<Integer>();
+        for(int i=0; i<9; i++) {
+            meetingIDs.add(test.createMeetingId());
+        }
+        assertEquals(test.createMeetingId(), 10);
     }
     
     
