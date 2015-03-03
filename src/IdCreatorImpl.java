a38c6de133077490fc521c7f5ecbd6826d00bd85
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-03-03 15:49:19 +0000

    updated implementation with createMeetingId()

--- src/IdCreatorImpl.java
+++ src/IdCreatorImpl.java
@@ -1,21 +1,43 @@
 /**
- * Implementation of {@see IdCreator}. 
- * It issues unique IDs, from 1000 upwards (to try and maintain a four-digits standard).
+ * Implementation of {@see IdCreator} interface. It creates ID numbers for a creation 
+ * of a new Meeting and Contact. Classes using this implementation should make sure
+ * to use the same unique object in order to have a different id number for any method call,
+ * as the class is not static.
  * 
  * @author federico.bartolomei (BBK-PiJ-2014-21) 
  */
 public class IdCreatorImpl implements IdCreator {
-    private int id = 999;
+    private int contactID = 999;
+    private int meetingID = 0;
 
     /**
      * {@inheritDoc}
      * 
-     * @return an unique id.
+     * Creates a new unique id to be associated with a Contact.
+     * It issues numbers from 1000 upwards (to try and maintain a four-digits standard).
+     * 
+     * @return an unique id for a new Contact.
      */
-    public int createId() {
-        id++;
-        return id;
+    @Override
+    public int createContactId() {
+        contactID++;
+        return contactID;
     }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * Creates a new unique id to be associated with a Meeting.
+     * It issues numbers from 1 upwards. 
+     *   
+     * @return an unique id for a new Meeting.
+     */
+    @Override
+    public int createMeetingId() {
+        meetingID++;
+        return meetingID;
+    }
+    
     
 }    
 
