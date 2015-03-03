1d3f5de41371b4bd16a298f0e4e0353d3ecba45c
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-03-03 14:48:54 +0000

    modified interface - added createMeetingId()

--- src/IdCreator.java
+++ src/IdCreator.java
@@ -1,16 +1,26 @@
 /**
- * Interface to issue unique IDs, created to support the implementation of {@see Contact}.
+ * Interface to issue unique IDs, created to support the implementation of {@see Contact} and {@see Meeting}.
  * 
  * @author federico.bartolomei (BBK-PiJ-2014-21)
  */
 public interface IdCreator {
 
     /**
-     * Create an unique ID number. The implementation should guarantee that a different number
-     * is returned every time the method is called.
+     * Create an unique ID number to be associated with a Contact. 
+     * The implementation should guarantee that a different number is returned 
+     * every time the method is called.
      *
      * @return an unique id number.
      */
-    public int createId();
+    public int createContactId();
+
+    /**
+     * Create an unique ID number to be associated with a Meeting. 
+     * The implementation should guarantee that a different number is returned 
+     * every time the method is called.
+     *  
+     * @return an unique id number for the Meeting.
+     */
+    public int createMeetingId();
     
 }
