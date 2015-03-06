90a370c5e0c26b1523f55188752353ae9cedbdfc
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-03-06 17:41:01 +0000

    updated PastMeeting test (exception handling)

--- src/test/TestPastMeeting.java
+++ src/test/TestPastMeeting.java
@@ -1,5 +1,9 @@
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.internal.matchers.Null;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -7,11 +11,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * JUnit test class for interface {@see PastMeeting}. It assumes an implementation called PastMeetingImpl.
- * Null arguments in the constructor, in addNotes(String) and getNotes(String) are not tested as these
- * conditions should throw an exception from the caller class ContactManager.
  *
  * @author federico.bartolomei (BBK-PiJ-2014-21)
  */
@@ -25,15 +29,60 @@ public class TestPastMeeting {
         date = new GregorianCalendar(2015,10,21,10,15);
     }
 
+    @After
+    public void tearDown() {
+        contacts = null;
+        date = null;
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    public Contact createContactMock() {
+        Contact dummy = mock(Contact.class);
+        when(dummy.getName()).thenReturn("Contact1");
+        when(dummy.getId()).thenReturn(1000);
+        when(dummy.getNotes()).thenReturn("Some notes");
+        return dummy;
+    }
+
+    @Test
+    public void testNullDateShouldThrowNullPointerException() {
+        exception.expect(NullPointerException.class);
+        PastMeeting nullDate = new PastMeetingImpl(null, contacts, 10, "Stringy");
+    }
+
+    @Test
+    public void testNullContactsShouldThrowNullPointerException() {
+        exception.expect(NullPointerException.class);
+        PastMeeting nullContacts = new PastMeetingImpl(date, null, 10);
+    }
+
+    @Test
+    public void testEmptyContactSetShouldThrowIllegalArgumentException() {
+        exception.expect(IllegalArgumentException.class);
+        contacts.add(createContactMock());
+        PastMeeting empty = new PastMeetingImpl(date, contacts, 10);
+    }
+
+    @Test
+    public void testNullNotesShouldThrowNullPointerException() {
+        exception.expect(NullPointerException.class);
+        contacts.add(createContactMock());
+        PastMeeting nullNotes = new PastMeetingImpl(date, contacts, 10, null);
+    }
+
     @Test
     public void testGetNotesEmptyString() {
         PastMeeting empty = new PastMeetingImpl(date, contacts, 10);
+        contacts.add(createContactMock());
         assertEquals(empty.getNotes(), "");
     }
 
     @Test
     public void testGetNotes() {
         PastMeeting test = new PastMeetingImpl(date, contacts, 10, "Notes about a past meeting");
+        contacts.add(createContactMock());
         assertEquals(test.getNotes(), "Notes about a past meeting");
     }
 
